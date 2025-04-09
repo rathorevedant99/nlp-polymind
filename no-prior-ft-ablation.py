6565b58f51eff5fd68c73cf4efc81e80227ab305
rathorevedant99 <rathorevedant99@gmail.com>
2025-04-09 23:31:16 +0100

    All experiments listed

--- no-prior-ft-ablation.py
+++ no-prior-ft-ablation.py
@@ -13,6 +13,8 @@ from src.utils.plot_exp import plot_expert_run_performance, plot_expert_summary
 import logging
 import pandas as pd
 from tqdm import tqdm
+import os
+import json
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -39,11 +41,17 @@ def main(config: DictConfig):
     Loads the config and runs the experiment.
     """
     runs = 10
-
     data = pd.DataFrame(columns=["run", "expert_id", "before", "after"])
-
     hydra_output_path = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
+    data_json_path = hydra_output_path + "/run_data.json"
+
     for run in tqdm(range(runs), desc="Run"):
+        if os.path.exists(data_json_path):
+            with open(data_json_path, "r") as f:
+                data_json = json.load(f)
+        else:
+            data_json = {}
+
         arranger = Arranger(config)
         expert_datasets, eval_data, test_data = arranger.create_datasets()
 
@@ -58,7 +66,7 @@ def main(config: DictConfig):
 
         logger.info("Starting debate")
 
-        shuffled_eval_data = eval_data.shuffle()
+        shuffled_eval_data = eval_data.shuffle(seed=run)
         if config.data.name == "samsum":
             tasks = [task_set["dialogue"] for task_set in shuffled_eval_data]
             ground_truths = [task_set["summary"] for task_set in shuffled_eval_data]
@@ -73,13 +81,14 @@ def main(config: DictConfig):
         
         metrics = Metrics()
 
+
         test_data = test_data.select(range(10))
         test_tasks = [task_set["dialogue"] for task_set in test_data]
         test_ground_truths = [task_set["summary"] for task_set in test_data]
 
         before_expert_scores = expert_test_evaluation(team, test_tasks, test_ground_truths, metrics)
             
-        memory = debate.execute_debate(tasks, ground_truths)
+        memory = debate.execute_debate(tasks, ground_truths, append=False)
         del debate
         del critic
 
@@ -89,6 +98,13 @@ def main(config: DictConfig):
             expert.memory_fine_tuning(instruction_data)
 
         after_expert_scores = expert_test_evaluation(team, test_tasks, test_ground_truths, metrics)
+
+        data_json[run+1] = {
+            "before": before_expert_scores,
+            "after": after_expert_scores
+        }
+
+        json.dump(data_json, open(data_json_path, "w"), indent=2)
 
         for i in range(len(before_expert_scores)):
             for expert_idx in range(len(before_expert_scores[i])):
